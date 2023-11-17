8ca3c425e546cea1e71c3b81e1149e15da4068db
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-11-17 10:41:14 +0300

    commit

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -36,6 +36,90 @@ export default (app) => {
   app.disable('etag');
 
   app.use(rateLimiter);
+
+  /// MACHINE 1
+/////////////
+var triggerCounter = 0;
+var triggerTimeList = [];
+var triggeredClients = [];
+
+var trigger = false;
+
+var email = '';
+var deviceType = '';
+var name = '';
+
+app.post("/checkTrigger", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+
+  let billboardType = req.body.billboard;
+  var result = false;
+  if (billboardType == "1") {
+    result = trigger;
+  } else if (billboardType == "2") {
+    result = trigger2;
+  }
+  res.json({
+    status: result,
+    email: email,
+    name: name,
+    deviceType: deviceType
+  });
+});
+
+app.post("/trigger", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log('triggered');
+  let clientID = Math.floor(Math.random() * 999999);
+  let clientData = {
+    id: clientID,
+    status: true,
+  }
+  console.log(triggeredClients);
+  // if (triggeredClients.map((id) => id).includes(clientID) == false) {
+  //  triggeredClients.push(clientData);
+  // }
+  triggerCounter = triggerCounter + 1;
+  let ts = Date.now();
+  let date_ob = new Date(ts);
+  let date = date_ob.getDate();
+  let month = date_ob.getMonth() + 1;
+  let year = date_ob.getFullYear();
+  let hours = date_ob.getHours();
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
+  triggerTimeList[triggerTimeList.length] = `${year}/${month}/${date} ${hours}:${minutes}:${seconds}`
+
+  let billboardType = req.body.billboard;
+  email = req.body.email;
+  name = req.body.name;
+  deviceType = req.body.deviceType;
+  if (billboardType == "1") {
+    trigger = true;
+  } else if (billboardType == "2") {
+    trigger2 = true;
+  }
+
+  console.log('triggerCount : ' + triggerCounter);
+  console.log(`triggered in : ${triggerTimeList[triggerTimeList.length - 1]}`);
+  console.log(trigger);
+});
+
+app.post("/resetTrigger", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  let billboardType = req.body.billboard;
+  if (billboardType == "1") {
+    trigger = false;
+    email = '';
+    name = '';
+    deviceType = '';
+  } else if (billboardType == "2") {
+    trigger2 = false;
+  }
+  console.log('reset Trigger');
+  console.log(trigger);
+});
+/////////////
   let currentSkip = 0;
   // app.use(prefix, routes);
   const cache_384x960 = new NodeCache({ stdTTL: 15, deleteOnExpire: false });
